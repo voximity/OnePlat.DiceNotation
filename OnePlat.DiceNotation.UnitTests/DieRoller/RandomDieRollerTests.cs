01566ccf4167a68e487395002ae284f3166fe4ad
Pedro Silva <pedrosi@microsoft.com>
2017-08-07 10:28:17 -0700

    First checkin with the basic .NET Standard library, die roller, and unit tests.

---
+++ OnePlat.DiceNotation.UnitTests/DieRoller/RandomDieRollerTests.cs
@@ -0,0 +1,159 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnePlat.DiceNotation.DieRoller;
+using System;
+
+namespace OnePlat.DiceNotation.UnitTests.DieRoller
+{
+    /// <summary>
+    /// Summary description for RandomDieRollerTests
+    /// </summary>
+    [TestClass]
+    public class RandomDieRollerTests
+    {
+        public RandomDieRollerTests()
+        {
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void RandomDieRoller_DefaultConstructorTest()
+        {
+            // setup test
+
+            // run test
+            IDieRoller die = new RandomDieRoller();
+
+            // validate results
+            Assert.IsNotNull(die);
+            Assert.IsInstanceOfType(die, typeof(IDieRoller));
+            Assert.IsInstanceOfType(die, typeof(RandomDieRoller));
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_ConstructorRandomGeneratorTest()
+        {
+            // setup test
+            Random rand = new Random(42);
+
+            // run test
+            IDieRoller die = new RandomDieRoller();
+
+            // validate results
+            Assert.IsNotNull(die);
+            Assert.IsInstanceOfType(die, typeof(IDieRoller));
+            Assert.IsInstanceOfType(die, typeof(RandomDieRoller));
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld20Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(20);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 20);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld4Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(4);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 4);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld6Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(6);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 6);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld8Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(8);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 8);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld12Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(12);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 12);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld100Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(100);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 100);
+        }
+
+        [TestMethod]
+        public void RandomDieRoller_Rolld7Test()
+        {
+            // setup test
+            IDieRoller die = new RandomDieRoller();
+
+            // run test
+            int result = die.Roll(7);
+
+            // validate results
+            Assert.IsTrue(result >= 1 && result <= 7);
+        }
+    }
+}
---
+++ OnePlat.DiceNotation/DieRoller/RandomDieRoller.cs
@@ -0,0 +1,61 @@
+﻿// <copyright file="RandomDieRoller.cs" company="DarthPedro">
+// Copyright (c) 2017 DarthPedro. All rights reserved.
+// </copyright>
+
+//-----------------------------------------------------------------------
+// Assembly         : OnePlat.Mvvm.Core
+// Author           : DarthPedro
+// Created          : 8/7/2017
+//
+// Last Modified By : DarthPedro
+// Last Modified On : 8/7/2017
+//-----------------------------------------------------------------------
+// <summary>
+//       This project is licensed under the MS-PL license.
+//
+//       OnePlat.DiceNotation is an open source project that parses,
+//  evalutes, and rolls dice that conform to the defined Dice notiation.
+//  This notation is usable in any form of random dice games and role-playing
+//  games like D&D and d20.
+// </summary>
+//-----------------------------------------------------------------------
+using System;
+
+namespace OnePlat.DiceNotation.DieRoller
+{
+    /// <summary>
+    /// Die roller that rolls a random number between 1 and number of sides.
+    /// This uses the .NET framework random number generator (to its a
+    /// pseudo-random number).
+    /// </summary>
+    public class RandomDieRoller : IDieRoller
+    {
+        private static readonly Random DefaultRandomGenerator = new Random();
+
+        private Random random;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomDieRoller"/> class.
+        /// Uses the default static random number generator.
+        /// </summary>
+        public RandomDieRoller()
+            : this(DefaultRandomGenerator)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RandomDieRoller"/> class.
+        /// </summary>
+        /// <param name="random">Random number generator to use</param>
+        public RandomDieRoller(Random random)
+        {
+            this.random = random;
+        }
+
+        /// <inheritdoc/>
+        public int Roll(int sides)
+        {
+            return this.random.Next(sides) + 1;
+        }
+    }
+}
