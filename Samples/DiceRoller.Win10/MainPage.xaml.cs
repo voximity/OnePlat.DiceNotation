a1f1f1c439f4fe64ba72b18d74ebd75728979fe7
Pedro Silva <pedrosi@microsoft.com>
2017-08-25 17:10:41 -0700

    Create simple layout of Sample Win10 application with logos and images.

---
+++ Samples/DiceRoller.Win10/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="MainPage.xaml.cs" company="DarthPedro">
+// Copyright (c) 2017 DarthPedro. All rights reserved.
+// </copyright>
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+namespace DiceRoller.Win10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
