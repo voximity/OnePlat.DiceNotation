84d9dbd7f73fa265d6e4a9274bb1cfda742f2076
Pedro Silva <DarthPedro@gmail.com>
2017-08-28 22:13:37 -0700

    Created AppSettingsService with properties that can be persisted to roaming storage.

--- Samples/DiceRoller.Win10/Services/AppSettingsService.cs
+++ Samples/DiceRoller.Win10/Services/AppSettingsService.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace DiceRoller.Win10.Services
 {
@@ -15,5 +16,89 @@ namespace DiceRoller.Win10.Services
     /// </summary>
     public class AppSettingsService
     {
+        #region Members
+        private const string KeyUseDiceExpressionEditor = "UseDiceExpressionEditorKey";
+        private const bool DefaultUseDiceExpressionEditor = false;
+        private const string KeyUseUnboundedResults = "UseUnboundedResultsKey";
+        private const bool DefaultUseUnboundedResults = false;
+        private const string KeyDefaultDiceSides = "DefaultDiceSidesKey";
+        private const int DefaultDiceSidesDefault = 6;
+
+        private ApplicationDataContainer appSettings = ApplicationData.Current.RoamingSettings;
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to use the dice expression editor by default.
+        /// </summary>
+        public bool UseDiceExpressionEditor
+        {
+            get
+            {
+                return this.GetOrDefaultValue<bool>(KeyUseDiceExpressionEditor, DefaultUseDiceExpressionEditor);
+            }
+
+            set
+            {
+                this.appSettings.Values[KeyUseDiceExpressionEditor] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to allow unbounded dice results.
+        /// </summary>
+        public bool UseUnboundedResults
+        {
+            get
+            {
+                return this.GetOrDefaultValue<bool>(KeyUseUnboundedResults, DefaultUseUnboundedResults);
+            }
+
+            set
+            {
+                this.appSettings.Values[KeyUseUnboundedResults] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value to use for default dice sides, when dice sides are not
+        /// specified in dice expressions.
+        /// </summary>
+        public int DefaultDiceSides
+        {
+            get
+            {
+                return this.GetOrDefaultValue<int>(KeyDefaultDiceSides, DefaultDiceSidesDefault);
+            }
+
+            set
+            {
+                this.appSettings.Values[KeyDefaultDiceSides] = value;
+            }
+        }
+        #endregion
+
+        #region Helper methods
+
+        /// <summary>
+        /// Gets the value for the specified field from the roaming application settings.
+        /// If that key doesn't exist in the settings, then it returns the default value.
+        /// </summary>
+        /// <typeparam name="T">Type of return and default values</typeparam>
+        /// <param name="key">Key to find the setting</param>
+        /// <param name="defaultValue">Default value for the setting</param>
+        /// <returns>Returns the app settings value or the default.</returns>
+        private T GetOrDefaultValue<T>(string key, T defaultValue)
+        {
+            this.appSettings.Values.TryGetValue(key, out object value);
+            if (value == null)
+            {
+                value = defaultValue;
+            }
+
+            return (T)value;
+        }
+        #endregion
     }
 }
