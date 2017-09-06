8b95ea37446f2a192331127cd8a533cb29c43c77
Pedro Silva <DarthPedro@gmail.com>
2017-09-05 19:55:31 -0700

    Removed unused code from this library.

--- ExtendedDieRollers/ExtendedDieRollers.UnitTests/Helpers/AssertHelpers.cs
+++ ExtendedDieRollers/ExtendedDieRollers.UnitTests/Helpers/AssertHelpers.cs
@@ -9,25 +9,5 @@ namespace OnePlat.DiceNotation.UnitTests.Helpers
         {
             Assert.IsTrue(value >= min && value <= max);
         }
-
-        public static void AssertDiceChoose(DiceResult result, string expectedExpression,
-                                            string expectedDiceType, int expectedTotalResults,
-                                            int expectedAppliedResults, int modifier = 0)
-        {
-            Assert.AreEqual(expectedExpression, result.DiceExpression);
-            Assert.AreEqual(expectedTotalResults, result.Results.Count);
-            int sum = 0, count = 0;
-            foreach (TermResult r in result.Results)
-            {
-                Assert.IsTrue(r.Type.Contains(expectedDiceType));
-                if (r.AppliesToResultCalculation)
-                {
-                    sum += r.Value;
-                    count++;
-                }
-            }
-            Assert.AreEqual(sum + modifier, result.Value);
-            Assert.AreEqual(expectedAppliedResults, count);
-        }
     }
 }
