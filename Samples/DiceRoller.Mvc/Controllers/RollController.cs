a1368e90aef5879d497517191efae4dafe04e45a
Pedro Silva <pedrosi@microsoft.com>
2017-09-12 17:26:09 -0700

    Added simple RollController to provide die roll operations. Test implementation of controller methods. Got the two views set up.

---
+++ Samples/DiceRoller.Mvc/Controllers/RollController.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="RollController.cs" company="DarthPedro">
+// Copyright (c) 2017 DarthPedro. All rights reserved.
+// </copyright>
+
+using DiceRoller.Web.ViewModels;
+using OnePlat.DiceNotation;
+using OnePlat.DiceNotation.DiceTerms;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace DiceRoller.Mvc.Controllers
+{
+    /// <summary>
+    /// Controller class that handles die roll operations.
+    /// </summary>
+    public class RollController : Controller
+    {
+        /// <summary>
+        /// Default view of die roll results.
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult Index()
+        {
+            IList<DiceResult> results = new List<DiceResult>();
+            List<TermResult> rolls = new List<TermResult> { new TermResult { Scalar = 1, Value = 15, Type = "DiceTerm.d20" } };
+
+            results.Add(new DiceResult("d20", rolls, "RandomDieRoller", new DiceConfiguration()));
+
+            return this.View(results);
+        }
+
+        /// <summary>
+        /// Creates a die roll from dice expression.
+        /// GET: Roll/Create
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult Create()
+        {
+            return this.View(new DiceRollerViewModel());
+        }
+
+        /// <summary>
+        /// Creates a die roll from dice expression.
+        /// POST: Roll/Create
+        /// </summary>
+        /// <param name="collection">Forms collection.</param>
+        /// <returns>View</returns>
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                return this.RedirectToAction("Index");
+            }
+            catch
+            {
+                return this.View();
+            }
+        }
+    }
+}
---
+++ Samples/DiceRoller.Mvc/ViewModels/DiceRollerViewModel.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="DiceRollerViewModel.cs" company="DarthPedro">
+// Copyright (c) 2017 DarthPedro. All rights reserved.
+// </copyright>
+
+using OnePlat.DiceNotation;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DiceRoller.Web.ViewModels
+{
+    /// <summary>
+    /// View model for the dice roller pages.
+    /// </summary>
+    public class DiceRollerViewModel
+    {
+        /// <summary>
+        /// Gets or sets the dice expression text.
+        /// </summary>
+        [Display(Name = "Dice Expression")]
+        [Required]
+        public string DiceExpression { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of roll results.
+        /// </summary>
+        public IList<DiceResult> RollResults { get; set; } = new List<DiceResult>();
+    }
+}
